37c2e31414675434918f9558cc406fcecd82ad0e
HYUK LEE <ruldes76@gmail.com>
2020-05-13 09:32:47 +0900

    test

---
+++ queueNstackTest.rb
@@ -0,0 +1,26 @@
+queue = []
+
+# input data
+queue.push "tanaka"
+queue.push "suzuki"
+# queue #=> ["tanaka", "suzuki"]
+
+# output data
+puts queue.shift
+puts queue.shift
+p quere
+
+
+
+# stack
+stack = []
+
+stack.push "one"
+stack.push "two"
+stack.push "three"
+p stack
+
+puts stack.pop
+puts stack.pop
+puts stack.pop
+puts stack
